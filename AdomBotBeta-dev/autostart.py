01f685a9b2ecc82e517130340d66746352676cc3
MetraDemashov <a-demashov@ya.ru>
2026-06-23 12:41:23 +0300

    Delete autostart.py

--- AdomBotBeta-dev/autostart.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import time
-
-while True:
-    print("Запускаем бота...")
-    process = subprocess.Popen(["python", "d:/Work/Guchigengovo/bot.py"])
-    process.wait()  # ждем завершения процесса бота
-    print("Бот завершился. Перезапуск через 5 секунд...")
-    time.sleep(5)
